a9f17659af5d1747cbfb378622b3a4d17937de23
Ilia Rosseev <ilia19042006@gmail.com>
2024-01-07 01:16:30 +0300

    Создание структуры, приветствие бота

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Main')
+import telebot
+from telebot import types
+from config import BOT_TOKEN
+
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+printy = bot.send_message
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    printy(message.chat.id, f"Привет, {message.from_user.first_name}!")
+
+
+
+bot.polling(none_stop=True)
