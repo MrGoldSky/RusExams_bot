fd809a809ad4153115d9f9ffab3f353176fe6737
Ilia Rosseev <ilia19042006@gmail.com>
2024-01-07 16:58:41 +0300

    Случайные слова

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from telebot import types
 from base_class import *
 import app_logger
 from config import BOT_TOKEN
-
+import random
 
 
 bot = telebot.TeleBot(BOT_TOKEN)
@@ -48,7 +48,8 @@ def start(message):
     choice_questions = types.KeyboardButton(f"{chr(128290)}Выбор номера ЕГЭ{chr(128290)}")
     statistics = types.KeyboardButton(f"{chr(128161)}Статистика{chr(128161)}")
     top_users = types.KeyboardButton(f"{chr(128175)}Топ пользователей{chr(128175)}")
-    markup.add(choice_questions, bot_info, statistics, top_users)
+    start_solve = types.KeyboardButton(f"Начать решать!")
+    markup.add(choice_questions, bot_info, statistics, top_users, start_solve)
     printy(message.chat.id, f"Привет, {message.from_user.first_name}!")
     printy(message.chat.id, "Возможности бота:", reply_markup=markup)
 
@@ -70,20 +71,97 @@ def statistics(message):
     else:
         logger.critical(f"Connection to database(base) ERROR: userid={message.chat.id} exception={connection[0]}")
     try:
-        accepted = cur.execute(f"""SELECT accepted FROM user_base
-                                WHERE user_id == {message.chat.id}""").fetchone()[0]
-        wrong_answer = cur.execute(f"""SELECT wrong_answer FROM user_base
-                                WHERE user_id == {message.chat.id}""").fetchone()[0]
+
+        accepted = len(cur.execute(f"""SELECT id FROM solved
+                                WHERE user_id == {message.chat.id} AND solved == 1""").fetchall())
+        wrong_answer = len(cur.execute(f"""SELECT id FROM solved
+                                WHERE user_id == {message.chat.id} AND solved == 0""").fetchall())
         not_stated = cur.execute(f"""SELECT MAX(id) FROM question4""").fetchone()[0]
         con.close()
     except BaseException as e:
-        logger.error(f"Select from table (user_base) ERROR: values=accepted, wrong_answer, not_stated exception={e}")
+        logger.error(f"Select from table (solved, question4) ERROR: values=accepted, wrong_answer, not_stated exception={e}")
 
     printy(message.chat.id, f"""Статистика ЕГЭ №4\n
 Решено верно {chr(9989)} {accepted}
 Решено неверно {chr(10060)} {wrong_answer}
 Не решено {chr(9200)} {not_stated - accepted}""")
 
+
+@bot.message_handler(func=lambda message: message.text == f"Начать решать!")
+def solve(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+
+    not_solved = types.KeyboardButton(f"Решать нерешенные")
+    solved = types.KeyboardButton(f"Повторить решенные")
+    solve_any = types.KeyboardButton(f"Решать любые")
+    markup.add(not_solved, solve_any, solved)
+    printy(message.chat.id, "Что решаем?):", reply_markup=markup)
+    printy(message.chat.id, "Напиши: Стоп чтобы выйти из режима экзамена")
+
+
+@bot.message_handler(func=lambda message: message.text == f"Решать любые")
+def start_solve_any(message):
+    logger.debug(f"Reply solve(any) for userid={message.chat.id}")
+    connection = connect_to_db()
+    if len(connection) == 2:
+        con, cur = connection[0], connection[1]
+        logger.info(f"Connected to datebase(base) for userid={message.chat.id}")
+    else:
+        logger.critical(f"Connection to database(base) ERROR: userid={message.chat.id} exception={connection[0]}")
+    try:
+        ids = cur.execute(f"""SELECT id FROM question4""").fetchall()
+        solve_id = random.choice(ids)[0]
+        task = cur.execute(f"""SELECT task FROM question4 
+                               WHERE id == {solve_id}""").fetchone()[0]
+
+        correct = cur.execute(f"""SELECT correct FROM question4
+                                  WHERE id == {solve_id}""").fetchone()[0]
+        con.close()
+    except BaseException as e:
+        logger.error(f"Select from table (question4) ERROR: user_id={message.chat.id} values= ids, task, correct exception={e}")
+
+    bot.register_next_step_handler(printy(message.chat.id, f'{task}'), check, correct, "any", solve_id)
+
+
+def check(message, correct: str, type_solve: str, solve_id: int):
+    logger.debug(f'Checking for userid={message.chat.id} user_input={message.text}, correct={correct}')
+    connection = connect_to_db()
+    if len(connection) == 2:
+        con, cur = connection[0], connection[1]
+        logger.info(f"Connected to datebase(base) for userid={message.chat.id}")
+    else:
+        logger.critical(f"Connection to database(base) ERROR: userid={message.chat.id} exception={connection[0]}")
+    if message.text == "Стоп":
+        start(message)
+        return
+    if message.text == correct:
+        logger.debug(str(solve_id))
+        try:
+            if len(cur.execute(f"SELECT solved FROM solved WHERE task_id == {solve_id} AND user_id == {message.chat.id}").fetchall()):
+                cur.execute(f"""UPDATE solved SET solved = 1 WHERE task_id == {solve_id} AND user_id == {message.chat.id}""")
+            else:
+                cur.execute(f"""INSERT INTO solved(user_id, task_id, solved) 
+                            VALUES ({message.chat.id}, {solve_id}, 1) """)
+            con.commit()
+            con.close()
+        except BaseException as e:
+            logger.error(f"UPDATE table (solved) ERROR: values=solve_id, task_id, correct exception={e}")
+        printy(message.chat.id, f"{chr(128077)}")
+    else:
+        try:            
+            if len(cur.execute(f"SELECT solved FROM solved WHERE task_id == {solve_id} AND user_id == {message.chat.id}").fetchall()):
+                cur.execute(f"""UPDATE solved SET solved = 0 WHERE task_id == {solve_id} AND user_id == {message.chat.id}""")
+            else:
+                cur.execute(f"""INSERT INTO solved(user_id, task_id, solved) 
+                            VALUES ({message.chat.id}, {solve_id}, 0) """)
+            con.commit()
+            con.close()
+        except BaseException as e:
+            logger.error(f"UPDATE table (user_base) ERROR: values=solve_id, task, correct exception={e}")
+
+        printy(message.chat.id, f"{chr(128078)}")
+    if type_solve == 'any':
+        start_solve_any(message)
 
 bot.polling(none_stop=True)
 
