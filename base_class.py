51c316a3dd1ffc8fcbe8bdd862729ed84077b6c2
Ilia Rosseev <ilia19042006@gmail.com>
2024-01-07 20:14:54 +0300

    Топ пользователей

--- base_class.py
+++ base_class.py
@@ -3,7 +3,8 @@ import sqlite3
 
 def connect_to_db():
     try:
-        con = sqlite3.connect('base/base.sqlite3')
+        # con = sqlite3.connect('base/base.sqlite3')
+        con = sqlite3.connect('C:\\Users\Mr_GoldSky_\\Desktop\\RusExams_bot\\base\\base.sqlite3')
         cur = con.cursor()
         return con, cur
     except BaseException as e:
--- main.py
+++ main.py
@@ -11,13 +11,9 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 printy = bot.send_message
 
-# logging.debug("A DEBUG Message")
-# logging.info("An INFO")
-# logging.warning("A WARNING")
-# logging.error("An ERROR")
-# logging.critical("A message of CRITICAL severity")
 cwd = os.getcwd()
-os.chdir(f'logs')
+# os.chdir(f'logs')
+os.chdir('C:\\Users\\Mr_GoldSky_\\Desktop\\RusExams_bot\\logs')
 logger = app_logger.get_logger(__name__)
 os.chdir(cwd)
 logger.info('Logger successfully installed')
@@ -33,8 +29,8 @@ def start(message):
     else:
         logger.critical(f"Connection to database(base) ERROR: userid={message.chat.id} exception={connection[0]}")
     try:
-        cur.execute(f"""INSERT INTO user_base(user_id, user_name)
-                        VALUES ({message.chat.id}, '{message.from_user.first_name}')""")
+        cur.execute(f"""INSERT INTO user_base(user_id, user_name, username)
+                        VALUES ({message.chat.id}, '{message.from_user.first_name}', '{message.from_user.username}')""")
         con.commit()
         con.close()
     except BaseException as e:
@@ -140,7 +136,6 @@ def check(message, correct: str, type_solve: str, solve_id: int):
         start(message)
         return
     if message.text == correct:
-        logger.debug(str(solve_id))
         try:
             if len(cur.execute(f"SELECT solved FROM solved WHERE task_id == {solve_id} AND user_id == {message.chat.id}").fetchall()):
                 cur.execute(f"""UPDATE solved SET solved = 1 WHERE task_id == {solve_id} AND user_id == {message.chat.id}""")
@@ -168,6 +163,48 @@ def check(message, correct: str, type_solve: str, solve_id: int):
         printy(message.chat.id, f"{correct.lower()} - {correct}", reply_markup=markup)
     if type_solve == 'any':
         start_solve_any(message)
+
+
+@bot.message_handler(func=lambda message: message.text == f"{chr(128175)}Топ пользователей{chr(128175)}")
+def top(message):
+    logger.debug(f"Reply top for userid={message.chat.id}")
+    connection = connect_to_db()
+    if len(connection) == 2:
+        con, cur = connection[0], connection[1]
+        logger.info(f"Connected to datebase(base) for userid={message.chat.id}")
+    else:
+        logger.critical(f"Connection to database(base) ERROR: userid={message.chat.id} exception={connection[0]}")
+    try:
+        current_top = []
+
+        for user_id in cur.execute(f"""SELECT DISTINCT user_id FROM solved""").fetchall():
+            current_accepted = len(cur.execute(f"""SELECT id FROM solved
+                                    WHERE user_id == {user_id[0]} AND solved == 1""").fetchall())
+            current_wrong_answer = len(cur.execute(f"""SELECT id FROM solved
+                                    WHERE user_id == {user_id[0]} AND solved == 0""").fetchall())
+            current_username = cur.execute(f"""SELECT username FROM user_base
+                                    WHERE user_id == {user_id[0]}""").fetchone()[0]
+            current_name = cur.execute(f"""SELECT user_name FROM user_base
+                                    WHERE user_id == {user_id[0]}""").fetchone()[0]
+            current_not_stated = cur.execute(f"""SELECT MAX(id) FROM question4""").fetchone()[0] - current_accepted - current_wrong_answer
+            current_top.append({"user_id": user_id[0], "current_accepted": current_accepted, "current_wrong_answer": current_wrong_answer, 
+                                "current_not_stated": current_not_stated, "username": current_username, 'name': current_name})
+        con.close()
+    except BaseException as e:
+        logger.error(f"Select from table (solved) ERROR: values=user_id exception={e}")
+    current_top = sorted(current_top, key=lambda x:(-x['current_accepted'], x['current_not_stated'], x['current_wrong_answer']))
+    text = f"Топ пользователей по решению\!\n"
+    
+    if len(current_top) < 3:
+        n = len(current_top)
+    else:
+        n = 3
+    rewards = {1:129351, 2:129352, 3:129353}
+    for i in range(n):
+        text += f'{chr(rewards[i + 1])} [{current_top[i]["name"]}](https://t.me/{current_top[i]["username"]})\n'
+        text += f'Решено верно{chr(9989)}: {current_top[i]["current_accepted"]} Решено неверно{chr(10060)}: {current_top[i]["current_wrong_answer"]}\n'
+    printy(message.chat.id, text, parse_mode='MarkdownV2', disable_web_page_preview=True)
+
 
 bot.polling(none_stop=True)
 
